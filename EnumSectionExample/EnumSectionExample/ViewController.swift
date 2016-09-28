bca10fc14bb672b470711d6fd943cd926cd9d67d
Arnas Dundulis <arnas@vinted.com>
2016-09-28 17:24:17 +0300

    Add empty project

---
+++ EnumSectionExample/EnumSectionExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EnumSectionExample
+//
+//  Created by Arnas Dundulis on 9/28/16.
+//  Copyright © 2016 whatever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
